1c907dc367ed970d1208fb6e8f244f817f023a67
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-25 14:37:35 +0200

    Avancement React

---
+++ src/components/Steps.js
@@ -0,0 +1,17 @@
+import { logDOM } from "@testing-library/react";
+import React from "react";
+
+class Steps extends React.Component {
+    render() {
+        return (
+            <div className="box col-sm-3 col-6">
+                <span className="material-icons" style={{ fontSize: "100", color: "black" }}>
+                    directions_walk
+                </span>
+            </div>
+        )
+    }
+
+}
+
+export default Steps;
